5fa5a7fc3666c9a8e40a8bfcf2d153cdc3bd7fc3
JelloMangune <mangune.jello@auf.edu.ph>
2023-08-11 03:39:54 +0800

    Added Login and Routing Middlewares for Security

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,14 +9,27 @@
     <title>Login</title>
 </head>
 <body style="background-color:#F2F8FD;">
-    <div class="container pt-5 px-5" >
-
-            <img src="images/AC_logo.png" class="rounded mx-auto d-block mt-5" height="250" alt="Angeles City logo">
+    <div class="container pt-5 px-5">
+        <img src="images/AC_logo.png" class="rounded mx-auto d-block mt-5" height="250" alt="Angeles City logo">
+        
+        <!-- Display error message if it exists -->
+        @if($errorMessage)
+        <div class="row justify-content-center">
+            <div class="col-10 col-md-6 col-lg-5">
+                <div class="alert alert-danger" role="alert">
+                    {{ $errorMessage }}
+                </div>
+            </div>
+        </div>
+        @endif
+        
+        <form action="{{ route('login') }}" method="POST">
+            @csrf
             <div class="row justify-content-center mt-5">
                 <div class="col-10 col-md-6 col-lg-5">
                     <div class="mb-3">
-                        <label for="userName" class="form-label">Username</label>
-                        <input type="email" class="form-control" id="userName" placeholder="Username">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" class="form-control" id="username" name="username" placeholder="Username">
                         <p id="usernameError" style="color: red; display: none;">Please enter a valid username.</p>
                     </div>
                 </div>
@@ -25,17 +38,18 @@
                 <div class="col-10 col-md-6 col-lg-5">
                     <div class="mb-3">
                         <label for="password" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="password" placeholder="Password">
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Password">
                         <p id="passwordError" style="color: red; display: none;">Please enter a valid password.</p>
                     </div>
                 </div>
             </div>  
             <div class="col d-flex mt-3 justify-content-center">
-                <input class="btn justify-content-center buttonColor" type="submit" value="Login" onclick="validateForm()">
+                <button class="btn justify-content-center buttonColor" type="submit">Login</button>
             </div>
-
+        </form>
     </div>
     
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-rTzBHQwb6a5STIIv73D4qa5SeJS6uNsfSlD1i6Mtpa/Irv3JxHQfa0bFMMNIp0bE" crossorigin="anonymous"></script>
 </body>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,61 +1,68 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\MainController;
+// use App\Http\Controllers\AdminController;
+// use App\Http\Controllers\UserController;
 
+use App\Http\Controllers\Admin\AdminDashboardController;
+use App\Http\Controllers\User\UserDashboardController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController; //sample
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/', [MainController::class, 'index']);
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Routes accessible only by user_type 0 (admin)
+Route::middleware(['check.user_type:0'])->prefix('admin')->group(function () {
+Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
+});
+
+// Routes accessible only by user_type 1 (regular user)
+Route::middleware(['check.user_type:1'])->prefix('user')->group(function () {
+Route::get('/dashboard', [UserDashboardController::class, 'index'])->name('user.dashboard');
+});
 
 
 
-Route::get('/', [AdminController::class,'main']);
-Route::get('/login', [AdminController::class,'login']);
-Route::get('/dashboard', [AdminController::class,'index']);
-Route::get('/barangay', [AdminController::class,'barangay']);
-Route::get('/addBarangay', [AdminController::class,'addBarangay']);
-Route::get('/addHistory', [AdminController::class,'addHistory']);
-Route::get('/editHistory', [AdminController::class,'editHistory']);
-Route::get('/history', [AdminController::class,'history']);
-Route::get('/viewHistory', [AdminController::class,'viewHistory']);
-Route::get('/addInfant', [AdminController::class,'addInfant']);
-Route::get('/editInfant', [AdminController::class,'editInfant']);
-Route::get('/infant', [AdminController::class,'infant']);
-Route::get('/viewInfant', [AdminController::class,'viewInfant']);
-Route::get('/addUser', [AdminController::class,'addUser']);
-Route::get('/editUser', [AdminController::class,'editUser']);
-Route::get('/userlist', [AdminController::class,'user']);
-Route::get('/addVaccine', [AdminController::class,'addVaccine']);
-Route::get('/editVaccine', [AdminController::class,'editVaccine']);
-Route::get('/vaccine', [AdminController::class,'vaccine']);
-Route::get('/viewVaccine', [AdminController::class,'viewVaccine']);
-Route::get('/upcoming', [AdminController::class,'upcoming']);
-Route::get('/missed', [AdminController::class,'missed']);
+// Route::get('/', [AdminController::class,'main']);
+// Route::get('/login', [AdminController::class,'login']);
+// Route::get('/dashboard', [AdminController::class,'index']);
+// Route::get('/barangay', [AdminController::class,'barangay']);
+// Route::get('/addBarangay', [AdminController::class,'addBarangay']);
+// Route::get('/addHistory', [AdminController::class,'addHistory']);
+// Route::get('/editHistory', [AdminController::class,'editHistory']);
+// Route::get('/history', [AdminController::class,'history']);
+// Route::get('/viewHistory', [AdminController::class,'viewHistory']);
+// Route::get('/addInfant', [AdminController::class,'addInfant']);
+// Route::get('/editInfant', [AdminController::class,'editInfant']);
+// Route::get('/infant', [AdminController::class,'infant']);
+// Route::get('/viewInfant', [AdminController::class,'viewInfant']);
+// Route::get('/addUser', [AdminController::class,'addUser']);
+// Route::get('/editUser', [AdminController::class,'editUser']);
+// Route::get('/userlist', [AdminController::class,'user']);
+// Route::get('/addVaccine', [AdminController::class,'addVaccine']);
+// Route::get('/editVaccine', [AdminController::class,'editVaccine']);
+// Route::get('/vaccine', [AdminController::class,'vaccine']);
+// Route::get('/viewVaccine', [AdminController::class,'viewVaccine']);
+// Route::get('/upcoming', [AdminController::class,'upcoming']);
+// Route::get('/missed', [AdminController::class,'missed']);
 
 
-Route::get('/user/dashboard', [UserController::class,'indexUser']);
-Route::get('/user/addHistory', [UserController::class,'addHistoryUser']);
-Route::get('/user/editHistory', [UserController::class,'editHistoryUser']);
-Route::get('/user/history', [UserController::class,'historyUser']);
-Route::get('/user/viewHistory', [UserController::class,'viewHistoryUser']);
-Route::get('/user/addInfant', [UserController::class,'addInfantUser']);
-Route::get('/user/editInfant', [UserController::class,'editInfantUser']);
-Route::get('/user/infant', [UserController::class,'infantUser']);
-Route::get('/user/viewInfant', [UserController::class,'viewInfantUser']);
-Route::get('/user/upcoming', [UserController::class,'upcomingUser']);
-Route::get('/user/missed', [UserController::class,'missedUser']);
+// Route::get('/user/dashboard', [UserController::class,'indexUser']);
+// Route::get('/user/addHistory', [UserController::class,'addHistoryUser']);
+// Route::get('/user/editHistory', [UserController::class,'editHistoryUser']);
+// Route::get('/user/history', [UserController::class,'historyUser']);
+// Route::get('/user/viewHistory', [UserController::class,'viewHistoryUser']);
+// Route::get('/user/addInfant', [UserController::class,'addInfantUser']);
+// Route::get('/user/editInfant', [UserController::class,'editInfantUser']);
+// Route::get('/user/infant', [UserController::class,'infantUser']);
+// Route::get('/user/viewInfant', [UserController::class,'viewInfantUser']);
+// Route::get('/user/upcoming', [UserController::class,'upcomingUser']);
+// Route::get('/user/missed', [UserController::class,'missedUser']);
 
-Route::get('/dashboard1', [DashboardController::class, 'index'])->name('dashboard'); //sample
 
 
 
